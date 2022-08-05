cd2245049ce8c645987cd34c81458344e5b0c990
zile028 <zile028@gmail.com>
2022-08-06 00:36:13 +0200

    fix design

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import headerBg from "../assets/img/header-bg.jpg";
 
 function Header({ title }) {
   const style = {
-    background: `linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(${headerBg})`,
+    backgroundImage: `linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(${headerBg})`,
   };
   return (
     <div className="header w-100" style={style}>
